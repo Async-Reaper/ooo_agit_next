f46415d7695fa9445731e3f74133789a137a6b41
ARSync <63187302+Async-Reaper@users.noreply.github.com>
2025-02-04 17:55:00 +0700

    Change image for link on cases page

--- src/app/cases/page.tsx
+++ src/app/cases/page.tsx
@@ -10,7 +10,8 @@ export const metadata: Metadata = {
       title: "Наши кейсы",
       description: "Мы расскажем о каждом кейсе, включая вызовы, с которыми мы столкнулись, и решения, которые мы предложили.",
       url: "https://ooo-agit-next.vercel.app/",
-      images: "https://ooo-agit-next.vercel.app/logoForSeo.png"
+      // eslint-disable-next-line max-len
+      images: "https://img.freepik.com/free-vector/photos-concept-illustration_114360-196.jpg?t=st=1738666463~exp=1738670063~hmac=f8bb2df63f6abcc73f92cb8e252ffa8e68fd4bfebf06e4c3f9ac94ad08f85a73&w=740"
    }
 };
 
