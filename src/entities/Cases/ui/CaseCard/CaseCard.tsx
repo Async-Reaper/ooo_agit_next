e29d8f45368bda32c4b308e37972d4a807b2e563
Professional <m_igor_r@mail.ru>
2025-03-17 17:38:58 +0700

    fix(cases) * Editing the switch positioning in cases; * Adding images to the background of cases; * Optimizing images (compression, conversion to webp); * Editing the size and positioning of the case container; * Adding glow to case images; * Editing animation: replacing images, optimizing images (compression, conversion to webp)

--- src/entities/Cases/ui/CaseCard/CaseCard.tsx
+++ src/entities/Cases/ui/CaseCard/CaseCard.tsx
@@ -1,10 +1,11 @@
 "use client";
 import React from "react";
+import { classNames } from "@shared/libs/classNames/classNames";
 import { Typography } from "@shared/ui";
 
 import cls from "./CaseCard.module.scss";
 
-import { ICase } from "../../model/types/casesTypes";
+import { CaseTypes, ICase } from "../../model/types/casesTypes";
 
 interface CaseCardProps {
   caseItem: ICase;
@@ -12,11 +13,17 @@ interface CaseCardProps {
 
 export const CaseCard = React.memo(({ caseItem }: CaseCardProps) => {
 
+  const mods = {
+    [cls.primary]: caseItem.type === CaseTypes.AUTOMATION,
+    [cls.secondary]: caseItem.type === CaseTypes.INTEGRATION,
+    [cls.tertiary]: caseItem.type === CaseTypes.WEB_TECH,
+  };
+
   return (
-    <div className={cls.case_card__wrapper}>
-      {
-        caseItem.img && <img src={caseItem.img} alt="" className={cls.case__img} />
-      }
+    <div className={classNames(cls.case_card__wrapper, mods)}>
+      <div className={cls.case__img}>
+        <img src={caseItem.img} alt="" />
+      </div>
       <div className={cls.case__content}>
         <div className={cls.case__title}>
           <Typography variant="h2" bold>
