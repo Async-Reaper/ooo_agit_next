8a42b91694f9972d9154c0819bb630553e492915
u-isp <m_igor_r@mail.ru>
2025-05-27 17:54:40 +0700

    fix(non auth user)
    Add validate for non auth user (if user non auth, then his auto navigate to login page)

--- src/app/(LayoutSecond)/admin/page.tsx
+++ src/app/(LayoutSecond)/admin/page.tsx
@@ -1,14 +1,22 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import { UserRole } from "@entities/User";
 import { AddTaskButton } from "@features/AddTask";
 import { TasksList } from "@widgets/TasksList";
-import { useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 
 const Page = () => {
   const searchParams = useSearchParams();
   const userRole = searchParams?.get("role");
-  
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!localStorage.getItem("userId")) {
+      router.push("/admin/login");
+    }
+  }, [router]);
+
   return (
     <div>
       {
